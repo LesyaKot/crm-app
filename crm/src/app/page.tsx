a21bd9d6d866f5acd679f132c376ec60d74a501b
LesyaKot <lesyakot83@gmail.com>
2024-11-14 17:25:47 +0500

    add css

--- crm/src/app/page.tsx
+++ crm/src/app/page.tsx
@@ -1,9 +1,17 @@
 import styles from './page.module.css';
+import StatusLabel, {Status} from "@/app/components/status-label";
+
 
 export default function Home() {
   return (
          <main className={styles.main}>
-             <h1>Home page</h1>
+             <h1 className="text-xl">Home page</h1>
+             <StatusLabel status={Status.Active}>Active</StatusLabel>
+             <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+             <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+             <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+
+
               </main>
 
 

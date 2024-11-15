dcbe4c3d2b3dca0d384aa43ae58e5bb1c6df7127
LesyaKot <lesyakot83@gmail.com>
2024-11-15 17:02:27 +0500

    add dynamic elements

--- crm/src/app/page.tsx
+++ crm/src/app/page.tsx
@@ -1,19 +1,18 @@
 import styles from './page.module.css';
 import StatusLabel, {Status} from "@/app/components/status-label";
+import AddCompanyButton from "@/app/components/add-company-button";
 
 
 export default function Home() {
   return (
          <main className={styles.main}>
              <h1 className="text-xl">Home page</h1>
-             <StatusLabel status={Status.Active}>Active</StatusLabel>
-             <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
-             <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-             <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+             {/*<StatusLabel status={Status.Active} disabled={false}>Active</StatusLabel>*/}
+             {/*<StatusLabel status={Status.NotActive} disabled={false}>Not Active</StatusLabel>*/}
+             {/*<StatusLabel status={Status.Pending} disabled={false}>Pending</StatusLabel>*/}
+             {/*<StatusLabel status={Status.Suspended} disabled={false}>Suspended</StatusLabel>*/}
 
-
-              </main>
-
-
+             <AddCompanyButton />
+         </main>
   );
 }

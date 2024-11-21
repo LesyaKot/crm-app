e0cc8c7fb882aba93bea154c5da8daba5839453c
LesyaKot <lesyakot83@gmail.com>
2024-11-21 15:41:55 +0500

    add query fetch

--- crm/src/app/components/company-table.tsx
+++ crm/src/app/components/company-table.tsx
@@ -1,7 +1,13 @@
+'use client';
+
 import React from 'react';
-export interface CompanyTableProps {
-  children?: React.ReactNode;
-}
+import { useQuery } from '@tanstack/react-query';
+import { getCompanies } from '@/lib/api';
+import CompanyRow from '@/app/components/company-row';
+
+
+export interface CompanyTableProps {}
+
 const headers = [
   'Category',
   'Company',
@@ -10,20 +16,30 @@ const headers = [
   'Country',
   'Joined date',
 ];
-export default function CompanyTable({ children }: CompanyTableProps) {
+export default function CompanyTable({}: CompanyTableProps) {
+  const { data } = useQuery({
+    queryKey: ['companies'],
+    queryFn: () => getCompanies(),
+    staleTime: 10 * 1000,
+  });
+
   return (
     <div className="py-8 px-10 bg-gray-100">
       <table className="table-auto w-full border-separate border-spacing-y-2">
         <thead>
-          <tr>
-            {headers.map((header, i) => (
+        <tr>
+          {headers.map((header, i) => (
               <th key={i} className="pb-5 text-sm font-light text-gray-900">
                 {header}
               </th>
-            ))}
-          </tr>
+          ))}
+        </tr>
         </thead>
-        <tbody>{children}</tbody>
+        <tbody>
+        {data?.map((company) => (
+            <CompanyRow key={company.id} company={company}/>
+        ))}
+        </tbody>
       </table>
     </div>
   );
--- crm/src/app/layout.tsx
+++ crm/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Plus_Jakarta_Sans} from 'next/font/google';
+import Providers from '@/app/components/providers';
 import './globals.css';
 
 const font = Plus_Jakarta_Sans({subsets:['latin']});
@@ -10,8 +11,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }){
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
+      <html lang="en">
+      <body className={font.className}>
+      <Providers>{children}</Providers>
+      </body>
+      </html>
   );
 }
